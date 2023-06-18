e531bd5cb192e7b199ff370d31d4e8c1540578a8
fake-dp <fakecoko92@gmail.com>
2023-06-19 04:19:43 +0900

    Feat : Oauth 구글 로그인 진행중

--- src/components/header/atoms/GoogleBtn.tsx
+++ src/components/header/atoms/GoogleBtn.tsx
@@ -1,10 +1,39 @@
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { GoogleBtnWrapper } from "../../../styles/headerStyled/HeaderStyled";
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const GoogleBtn = () => {
+  const navigate = useNavigate();
+
+  const googleLogin = () => {
+    window.location.href =
+      "http://ec2-3-39-180-100.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorize/google";
+  };
+
+  const handleCallback = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const authorizationCode = urlParams.get("code");
+
+    try {
+      const response = await axios.post("/oauth2/callback", {
+        code: authorizationCode,
+      });
+      console.log(response);
+      navigate("/");
+      // const accessToken = response.data.accessToken;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleCallback();
+  }, []);
+
   return (
     <GoogleBtnWrapper>
-      <div>
+      <div onClick={googleLogin}>
         <AiFillGoogleCircle />
         <span> 구글 로그인</span>
       </div>
